5268a72f125c10e48babae75ac06e919514ea680
Hassan kheled <Hassankheled006@gmail.com>
2022-12-12 06:52:27 +0200

    Turn XML into Array

--- XMLConversion.cpp
+++ XMLConversion.cpp
@@ -3,10 +3,13 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include<string>
 #include <algorithm>
 #include <cctype>
 #include <vector>
+#include <list>
+
 
 using namespace std;
 
@@ -14,19 +17,74 @@ using namespace std;
 int main()
 {
     string x;
+    string y;
 
-    ifstream f("XML.txt");            
-    ofstream fileCreated("Testf.txt"); 
+    ifstream f("XML.txt");
+    ofstream fileCreated("Testf.txt");
 
 
-    while (getline(f,x)) {
+    vector<Node*> allNodes = vector<Node*>();
 
-        x.erase(remove_if(x.begin(), x.end(), ::isspace), x.end());  
-        fileCreated << x << "\n";
+    while (getline(f, x)) {
+        stringstream ss;
+        for (int i = 0; i < x.length(); i++) {
+
+            if (x.at(i) == '<') {
+                i++;
+                bool ct = false;
+                if (x[i] == '/') {
+                    ct = true;
+                    i++;
+                }
+                while (x[i] != '>') {
+                    ss << x[i];
+                    i++;
+                }
+
+                Node* n = new Node(((ct) ? NodeType::CLOSINGTAG : NodeType::OPENINGTAG), ss.str());
+
+                allNodes.push_back(n);
+            }
+
+            else {
+
+                while (x[i] != '<')
+                    ss << x[i++];
+
+                Node* n = new Node(NodeType::DATA, ss.str());
+                allNodes.push_back(n);
+                i--;
+            }
+        }
+
     }
-    
-   
-    f.close();
-    fileCreated.close();
 }
+
+enum NodeType { OPENINGTAG, CLOSINGTAG, DATA, ELEMENT, REPEATEDTAG, DATAELEMENT };
+
+ static class Node {
+
+private:
+
+    NodeType* type;
+    string data;
+    vector <Node*> children;
+
+public:
+
+    Node() {
+        this->type = nullptr;
+        this->data = nullptr;
+        this->children = vector<Node*>();
+    }
+
+
+   Node(NodeType* t, std::string d) {
+        this->type = t;
+       this->data = d;
+        this->children = vector<Node*>();
+    }
+
+
+};
 
