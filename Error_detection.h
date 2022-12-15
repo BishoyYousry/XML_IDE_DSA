97b0f560fa07468b6442f729a93812d1a5202e9f
omarnkaram771 <omar.karam771@gmail.com>
2022-12-15 19:53:29 +0200

    add Error_detection.h file

---
+++ Error_detection.h
@@ -0,0 +1,12 @@
+#ifndef Error detection_H
+#define Error detection_H
+
+#include<string>
+#include<iostream>
+#include<fstream>
+#include<stack> 
+
+using namespace std;
+bool error_detection();
+
+#endif
