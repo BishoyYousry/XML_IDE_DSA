b653780fb5da4f49ac17162877a7b7b4ced967eb
zeyadMohamed20 <zeyadmohamedasu@gmail.com>
2023-01-29 16:20:23 +0200

    Add xml_minifying feature

---
+++ src/xml_minifying.cpp
@@ -0,0 +1,30 @@
+#include"xml_minifying.h"
+string xml_minifying(string fileStr)
+{
+	istringstream stream(fileStr);
+	string line;
+	string result = "";
+
+	while (getline(stream, line))
+	{
+		for (size_t i = 0; i < line.size(); i++)
+		{
+			if (line[i] == ' ')
+			{
+				if (i != 0 && line[i - 1] != '>' && i < line.size() - 1 && line[i + 1] != '<' && line[i + 1] != ' ') continue;
+				else
+				{
+					line.erase(i, 1);
+					i--;
+				}
+			}
+			else if (line[i] == '\n' || line[i] == '\t')
+			{
+				line.erase(i, 1);
+				i--;
+			}
+		}
+		result = result + line;
+	}
+	return result;
+}
