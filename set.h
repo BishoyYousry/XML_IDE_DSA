b82eea39f47b70881dd3165eb933ad6cf63dceb4
zeyadMohamed20 <zeyadmohamedasu@gmail.com>
2023-02-03 16:42:30 +0200

    Add Graph Network for XML with all features

---
+++ set.h
@@ -0,0 +1,16 @@
+#include <map>
+#include <vector>
+#include <iostream>
+
+class Set {
+private:
+	std::map<int, bool> elements;
+
+public:
+	void insert(int element);
+	void remove(int element);
+	bool exists(int element);
+	int size();
+	void print();
+	std::vector<int>getElements();
+};
